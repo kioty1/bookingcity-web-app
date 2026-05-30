ebe39310c660f31c997d5c85c6578eaa5fd8d44e
kioty1 <Maljub@taltech.ee>
2026-05-31 00:09:45 +0300

    Creating the website's home page and updating dependencies

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css';
+
+function App() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>Welcome to the BookingCity! 🏨</h1>
+    </div>
+  );
+}
+
+export default App;
